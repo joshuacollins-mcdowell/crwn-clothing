29be42153822aabc29cbf12d7d58c73beef6e42e
joshuacollins-mcdowell <joshua.collins-mcdowell@questanalytics.com>
2022-07-14 14:29:23 -0500

    Created checkout page

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,6 +1,7 @@
 import Button from '../button/button';
 import './cart-dropdown.scss';
 
+import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart';
 
@@ -8,13 +9,18 @@ import CartItem from '../cart-item/cart-item';
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate('/checkout')
+  }
 
   return (
     <div className='cart-dropdown-container'>
       <div className='cart-items'>
         {cartItems.map((item) => (<CartItem key={item.id} cartItem={item} />))}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
   )
 }
